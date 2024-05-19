fe652ac23df6c5d5ac59ccf88e7f362769bf700c
verma99776264 <167978647+verma99776264@users.noreply.github.com>
2024-05-19 12:43:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,41 @@ app.use(cors());
 
 let latestLocation = null;
 
+// Object to store the values with names as keys
+const locationData = {
+    munesh: {},
+    mayank: {},
+    lucky: {}
+};
+
 app.post('/update-location', (req, res) => {
-    latestLocation = req.body;
-    console.log('Location updated:', latestLocation);
-    res.sendStatus(200);
+    const { name, latestLocation } = req.body;
+    if (name === 'munesh' || name === 'mayank' || name === 'lucky') {
+        locationData[name] = latestLocation;
+        console.log('Location updated for', name, ':', latestLocation);
+        res.sendStatus(200);
+    } else {
+        res.status(400).send('Invalid name');
+    }
 });
 
-app.get('/get-location', (req, res) => {
-    res.json(latestLocation);
+
+
+app.get('/get-location/:name', (req, res) => {
+    const name = req.params.name;
+    if (name === 'munesh' || name === 'mayank' || name === 'lucky') {
+        const location = locationData[name];
+        if (location) {
+            res.json(location);
+        } else {
+            res.status(404).send('Location not found');
+        }
+    } else {
+        res.status(400).send('Invalid name');
+    }
 });
+
+
 
 // Define a route handler for the /display route
 app.get('/', (req, res) => {
@@ -28,6 +54,12 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/send', (req, res) => {
+    // Set the content type to HTML
+    res.setHeader('Content-Type', 'text/html');
+    // Send the HTML file as the response
+    res.sendFile(path.join(__dirname, 'send-location.html'));
+});
 
 
 const PORT = process.env.PORT || 3000;
