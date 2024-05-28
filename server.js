b498a548f77fb75819fc3f3b4090668c059d43f5
verma99776264 <167978647+verma99776264@users.noreply.github.com>
2024-05-28 22:13:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-let latestLocation = null;
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// Object to store the values with names as keys
-const locationData = {
-    munesh: {},
-    mayank: {},
-    lucky: {}
-};
+// WebSocket connection
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
 
+// Endpoint to update location
 app.post('/update-location', (req, res) => {
     const { name, latestLocation } = req.body;
-    if (name === 'munesh' || name === 'mayank' || name === 'lucky') {
-        locationData[name] = latestLocation;
-        console.log('Location updated for', name, ':', latestLocation);
-        res.sendStatus(200);
-    } else {
-        res.status(400).send('Invalid name');
-    }
+    io.emit('locationUpdate', { name, latestLocation });
+    res.sendStatus(200);
 });
 
-
-
-app.get('/get-location/:name', (req, res) => {
-    const name = req.params.name;
-    if (name === 'munesh' || name === 'mayank' || name === 'lucky') {
-        const location = locationData[name];
-        if (location) {
-            res.json(location);
-        } else {
-            res.status(404).send('Location not found');
-        }
-    } else {
-        res.status(400).send('Invalid name');
-    }
-});
-
-
-
-// Define a route handler for the /display route
-app.get('/', (req, res) => {
-    // Set the content type to HTML
-    res.setHeader('Content-Type', 'text/html');
-    // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, 'display-location.html'));
-});
-
-
-app.get('/send', (req, res) => {
-    // Set the content type to HTML
-    res.setHeader('Content-Type', 'text/html');
-    // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, 'send-location.html'));
-});
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
