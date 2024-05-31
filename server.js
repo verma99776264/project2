209dd1641abc9084b49d6fe0619556a2648e11e2
verma99776264 <167978647+verma99776264@users.noreply.github.com>
2024-05-31 11:20:01 +0530

    Update server.js 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const fs = require('fs');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,7 +18,20 @@ app.get('/display', (req, res) => {
     res.sendFile(__dirname + '/public/display.html');
 });
 
-// Route to send the updated location from the mobile device
+
+// Route to serve the HTML and JavaScript for sending location updates
+app.get('/sendlocation', (req, res) => {
+    // Read the HTML and JavaScript file for sending location updates
+    fs.readFile(__dirname + '/public/sendlocation.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading sendlocation.html:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.send(data);
+    });
+});
+
+// Route to handle the location update from the mobile device
 app.post('/send', (req, res) => {
     // Handle the location update from the mobile device and emit it to all connected clients
     // For demonstration purposes, let's assume the location is received in JSON format
@@ -26,6 +39,7 @@ app.post('/send', (req, res) => {
     io.emit('locationUpdate', location);
     res.sendStatus(200);
 });
+
 
 // Socket.IO connection
 io.on('connection', (socket) => {
